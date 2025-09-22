12affc245bbcc626a93b8eb416e328a397423fe7
glauzi <glauzibusiness@gmail.com>
2025-09-23 01:27:22 +0700

    Добавьте файлы проекта.

---
+++ BookManager.Console/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace BookManager.Console
+{
+    using BookManager.Core;
+    using System;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var testService = new TestServices();
+            Console.WriteLine(testService.GetTestMessage());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ BookManager.Core/TestServices.cs
@@ -0,0 +1,11 @@
+﻿namespace BookManager.Core
+{
+    public class TestServices
+    {
+        public string GetTestMessage()
+        {
+            return "Привет из Core! Библиотека работает!";
+        }
+
+    }
+}
