7c8c7daa83e3e3625dceca45c27707ce4373b33c
glauzi <glauzibusiness@gmail.com>
2025-10-24 10:56:29 +0700

    final

--- BookManager.DataAccessLayer/IBookRepository.cs
+++ BookManager.DataAccessLayer/IBookRepository.cs
@@ -7,37 +7,8 @@ namespace BookManager.DataAccessLayer
     /// Определяет контракт для репозитория работы с книгами
     /// Обеспечивает абстракцию над способом хранения данных
     /// </summary>
-    public interface IBookRepository
+    public interface IBookRepository : IRepository<Book>
     {
-        /// <summary>
-        /// Добавляет новую книгу в хранилище данных
-        /// </summary>
-        /// <param name="book">Книга для добавления</param>
-        void Add(Book book);
 
-        /// <summary>
-        /// Удаляет книгу по идентификатору из хранилища данных
-        /// </summary>
-        /// <param name="id">Идентификатор книги для удаления</param>
-        void Delete(int id);
-
-        /// <summary>
-        /// Возвращает все книги из хранилища данных
-        /// </summary>
-        /// <returns>Список всех книг</returns>
-        List<Book> GetAll();
-
-        /// <summary>
-        /// Находит книгу по идентификатору в хранилище данных
-        /// </summary>
-        /// <param name="id">Идентификатор книги для поиска</param>
-        /// <returns>Найденная книга или null если не найдена</returns>
-        Book GetById(int id);
-
-        /// <summary>
-        /// Обновляет информацию о книге в хранилище данных
-        /// </summary>
-        /// <param name="book">Книга с обновленными данными</param>
-        void Update(Book book);
     }
 }
---
+++ BookManager.DataAccessLayer/IRepository.cs
@@ -0,0 +1,43 @@
+﻿using BookManager.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookManager.DataAccessLayer
+{
+    public interface IRepository<T> where T : IDomainObject
+    {
+        /// <summary>
+        /// Добавляет новую сущность в хранилище данных
+        /// </summary>
+        /// <param name="entity">Сущность для добавления</param>
+        void Add(T entity);
+
+        /// <summary>
+        /// Удаляет сущность по идентификатору из хранилища данных
+        /// </summary>
+        /// <param name="id">Идентификатор сущности для удаления</param>
+        void Delete(int id);
+
+        /// <summary>
+        /// Возвращает все сущности из хранилища данных
+        /// </summary>
+        /// <returns>Список всех сущностей</returns>
+        List<T> GetAll();
+
+        /// <summary>
+        /// Находит сущность по идентификатору в хранилище данных
+        /// </summary>
+        /// <param name="id">Идентификатор сущности для поиска</param>
+        /// <returns>Найденная сущность или null если не найдена</returns>
+        T GetById(int id);
+
+        /// <summary>
+        /// Обновляет информацию о сущности в хранилище данных
+        /// </summary>
+        /// <param name="entity">Сущность с обновленными данными</param>
+        void Update(T entity);
+    }
+}
