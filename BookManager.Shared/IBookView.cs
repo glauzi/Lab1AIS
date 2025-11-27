20c9a9792e46b787278ded5f6160048073e4d0d3
glauzi <glauzibusiness@gmail.com>
2025-11-27 22:50:16 +0700

    final or not?

--- BookManager.Shared/IBookView.cs
+++ BookManager.Shared/IBookView.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using BookManager.Entities;
 using BookManager.Shared;
 
 namespace BookManager.Shared
--- BookManager.WinForms/Form1.cs
+++ BookManager.WinForms/Form1.cs
@@ -1,5 +1,4 @@
-﻿using BookManager.Entities;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using Ninject;
 using BookManager.Shared;
